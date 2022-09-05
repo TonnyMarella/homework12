9710477d9fe6b2f92cfaa1a4e0843ee6c64b3e01
KapOo <temazubkov02@gmail.com>
2022-09-05 10:58:58 +0300

    Complete(Changed)

--- commands/phone.py
+++ commands/phone.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from adressbook import AddressBook, get_name_and_phone
 
 
-def phone_command(command: str, adressbook, get_name_and_phone, record):
+def phone_command(command: str, adressbook: AddressBook, name_and_phone: get_name_and_phone, record):
     if command.split()[0] == 'add':  # Add contact
-        name, phone = get_name_and_phone()
+        name, phone = name_and_phone()
         birthday = input('Enter birthday(day month, year) or skip(enter):\n')
         try:
             birthday = datetime.strptime(birthday, "%d %B, %Y").date()
